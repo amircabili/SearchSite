5d467b214e0a0313183a542cf01e809bee6b2e0c
amir <amircabili@hotmail.com>
2019-12-25 16:22:59 +0200

    123

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { Employee } from '../models/employee.model';
 
 
 @Component({
@@ -11,14 +12,27 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  dateOfBirth: Date = new Date(2018, 1, 28);
+  // dateOfBirth: Date = new Date(2018, 1, 28);
   gender = 'male';
   isActive = true;
   department = 'HR';
   photoPath = '../assets/images/image1.png';
   previewPhoto = false;
 
-  datePickerConfig : Partial< BsDatepickerConfig >;
+  datePickerConfig : Partial < BsDatepickerConfig > ;
+
+  employee: Employee = {
+    id : null, 
+    name : null,
+    gender : null,
+    email : null,
+    phonenumber: null,
+    contactPreference : null,
+    dateOfBirth : null,
+    department : null,
+    isActive : null,
+    photoPath : null
+  };
 
   departments : Department[] = [
     {id:1, name:'Help Desk'},
@@ -32,15 +46,14 @@ export class CreateEmployeeComponent implements OnInit {
         dateInputFormat: 'DD-MM-YYYY',
         containerClass: 'theme-blue' ,
         isAnimated: true ,
-        showWeekNumbers: false
-      });
+       });
   }
 
   ngOnInit() { 
   }
 
-  saveEmployee(empForm: NgForm):void{
-      console.log(empForm.value); 
+  saveEmployee(newEmployee: Employee):void{
+      console.log(newEmployee); 
   }
 
   clearSearchInput(){
